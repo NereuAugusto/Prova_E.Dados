f3e4c0a76dbec7a4a57485e36154812517f2e3db
NereuAugusto <nereubeckercampos711@gmail.com>
2022-05-04 14:06:58 -0300

    Ajustes

--- ProvaParcial3sem/exercicio4/Lista.java
+++ ProvaParcial3sem/exercicio4/Lista.java
@@ -15,49 +15,52 @@ public class Lista {
 			inicio = ins;
 			fim = ins;
 			
-		} else if (inicio == fim){
+		} else if (inicio != fim){
 			if(ins.dado > fim.dado){
 				
-			fim.dir = ins;
-			ins.esq = fim;
-			fim = ins;
-				
-			} else {
-				
-			fim.esq = ins;
-			ins.dir = fim;
-			inicio = ins;
-			}
-			
-		} else {
-			if(ins.dado > fim.dado){
-				
-			fim.dir = ins;
-			ins.esq = fim;
-			fim = ins;
-				
-			} else if (ins.dado < inicio.dado) {
-				
-				inicio.esq = ins;
-				ins.dir = inicio;
-				inicio = ins;
-				
-			} else {
-				while(insRes != null){
-					if (ins.dado <= insRes.dir.dado && ins.dado >= insRes.dado){
+				fim.dir = ins;
+				ins.esq = fim;
+				fim = ins;
+					
+				} else if (ins.dado < inicio.dado) {
+					
+					inicio.esq = ins;
+					ins.dir = inicio;
+					inicio = ins;
+					
+				} else {
+					while(insRes != null){
+						if (ins.dado <= insRes.dir.dado && ins.dado >= insRes.dado){
+							
+						ins.esq = insRes;
+						insRes.dir.esq = ins;
+						ins.dir = insRes.dir;
+						insRes.dir = ins;
+						break;
 						
-					ins.esq = insRes;
-					insRes.dir.esq = ins;
-					ins.dir = insRes.dir;
-					insRes.dir = ins;
-					break;
-					
+						}
+						
+						insRes = insRes.dir;
 					}
-					
-					insRes = insRes.dir;
 				}
-			}
-		}
+
+		} else {
+			
+			if(ins.dado > fim.dado){
+				
+				fim.dir = ins;
+				ins.esq = fim;
+				fim = ins;
+					
+				} else {
+					
+				fim.esq = ins;
+				ins.dir = fim;
+				inicio = ins;
+				}
+				
+			
+					}
 	}
 	
 	// método para imprimir os elementos da lista
